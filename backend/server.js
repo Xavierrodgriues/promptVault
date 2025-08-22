39b6dc02719ee4a912ccda6e255d9e925a31dd75
xavier <xavierrodgriues123@gmail.com>
2025-08-23 00:13:45 +0530

    fix: server.js cookieParser moved up

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ app.use(cors({
     credentials: true,
 }));
 
+app.use(cookieParser());
 
 app.use(express.json());
-app.use(cookieParser());
 app.use("/auth", authRouter);
 app.use("/",promptRouter);
 app.use("/settings", settingsRouter);
