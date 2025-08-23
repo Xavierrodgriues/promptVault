998dc9a9c7a2f8f81a0269d77a8b3c91f4f2814a
xavier <xavierrodgriues123@gmail.com>
2025-08-23 11:17:38 +0530

    add: Updated maintenance page

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,7 +6,10 @@ import { ThemeProvider } from './context/ThemeContext.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <ThemeProvider>
     <BrowserRouter>
+    <div className='flex h-[100vh] w-[100vw] justify-center items-center'>
       <img src='../istockphoto-1348157796-612x612.jpg'/>
+
+    </div>
     </BrowserRouter>
   </ThemeProvider>
   ,
